1816164583304f4122d02676c82eef7dc4e4fa56
Nilufa Khan <khannilufa5@gmail.com>
2022-09-13 14:33:59 +0530

    completed getconnection method

--- src/main/java/com/jap/DeleteUpdateStudentData.java
+++ src/main/java/com/jap/DeleteUpdateStudentData.java
@@ -11,6 +11,7 @@ public class DeleteUpdateStudentData
 
     public Connection getConnection() throws ClassNotFoundException, SQLException{
 
+
             Connection connection = null;
             Class.forName("com.mysql.cj.jdbc.Driver");
             String url = "jdbc:mysql://localhost:3306/school";
@@ -18,29 +19,23 @@ public class DeleteUpdateStudentData
             String pass = "admin";
             connection = DriverManager.getConnection(url, user, pass);
             return connection;
-
-
-
     }
-
     public void deleteStudentData() throws SQLException, ClassNotFoundException {
       // call getConnection() method
        Statement st = getConnection().createStatement();
         //execute delete query
-        String sql = "Delete from student where rollno = 14";
+        String sql = "Delete from student where rollno = 29";
         st.executeUpdate(sql);
         System.out.println("Successfully deleted");
     }
-
     public void updateStudentData() throws SQLException,ClassNotFoundException{
         // call getConnection() method
         Statement st = getConnection().createStatement();
         //execute update query
-        String sql = "Update student set age = 30 where totalMarks = 678 and rollno = 15";
+        String sql = "Update student set age = 50 where totalMarks = 190 and rollno = 19";
         st.executeUpdate(sql);
         System.out.println("Successfully updated ");
     }
-
     public static void main(String[] args) {
         DeleteUpdateStudentData salesDataDemo = new DeleteUpdateStudentData();
         try {
